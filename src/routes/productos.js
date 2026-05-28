b9a38cf65836de729ad679cf85ea6feb98b09cf3
Jordan Araya <soseresyt@gmail.com>
2026-05-28 12:05:27 -0400

    Ultimo commit: Subida de informe en repositorio

--- src/routes/productos.js
+++ src/routes/productos.js
@@ -7,8 +7,6 @@ const auth = require('../middlewares/auth');
 // Endpoints 
 router.get('/', auth, controller.listarProductos); 
 router.post('/', auth, controller.crearProducto); 
-
-
 router.post('/eliminar/:id', auth, controller.eliminarProducto); 
 
 module.exports = router;
